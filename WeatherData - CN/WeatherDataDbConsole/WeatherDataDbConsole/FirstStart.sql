fd8809915d1220ac966d945cd5384ed7a5f5a97e
chrissen19 <chrissen19@gmail.com>
2022-01-07 11:02:04 +0100

    Add files via upload

---
+++ WeatherData - CN/WeatherDataDbConsole/WeatherDataDbConsole/FirstStart.sql
@@ -0,0 +1,45 @@
+USE Master
+GO
+
+CREATE DATABASE WeatherDataDb
+GO
+
+Use WeatherDataDb
+GO
+
+DROP TABLE [dbo].[WeatherData]
+GO
+
+CREATE TABLE [dbo].[WeatherData] (
+[Id] INT        IDENTITY (1,1) NOT NULL,
+[Datum] DATETIME NOT NULL,
+[Plats] varchar (10) NOT NULL,
+[Temperatur] DECIMAL (4,1) NOT NULL,
+[Luftfuktighet] INT NOT NULL);
+GO
+
+BULK INSERT WeatherData
+FROM 'C:\WeatherDataDbConsole\TempFuktData.csv'
+WITH
+(
+    FIRSTROW = 2, -- as 1st one is header
+    FIELDTERMINATOR = ',',  --CSV field delimiter
+    ROWTERMINATOR = '\n',   --Use to shift the control to next row
+    TABLOCK
+)
+GO
+
+DROP PROCEDURE [dbo].[SP_WEATHER_GET_LIST]
+GO
+
+CREATE PROCEDURE [dbo].[SP_WEATHER_GET_LIST]  
+AS  
+   BEGIN  
+   SELECT Id  
+                  ,Datum  
+                 ,Plats
+                 ,Temperatur
+                 ,Luftfuktighet
+   FROM weatherdata  
+END 
+GO
